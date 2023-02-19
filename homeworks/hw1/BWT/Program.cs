f2177de48033fce169947cb4d1786aa1b08e89f1
AmarskiyArtem <amarskiywork@gmail.com>
2023-02-19 20:21:00 +0300

    add fast reverse conversion func

--- homeworks/hw1/BWT/Program.cs
+++ homeworks/hw1/BWT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BWT
 {
@@ -12,25 +13,58 @@ namespace BWT
 
             for (var i = 1; i < shiftTable.Length; i++)
             {
-                char lastChar = shifts[originalString.Length - 1];
-                shifts = lastChar + shifts.Substring(0, shifts.Length - 1);
+                char firstChar = shifts[0];
+                shifts = shifts.Substring(1) + firstChar;
                 shiftTable[i] = shifts;
             }
             Array.Sort(shiftTable);
+            //Console.WriteLine("{0}", string.Join("\n", shiftTable));
             string result = "";
             for (var i = 0; i < shiftTable.Length; i++)
             {
                 result += shiftTable[i][shiftTable.Length - 1];
             }
-            resultStringIndex = Array.IndexOf(shiftTable, result) + 1;
+            resultStringIndex = Array.IndexOf(shiftTable, originalString);
             return result;
+        }
+
+        static string ReverseConversion(string convertedString, int tableIndex)
+        {
+            var count = new int[(int)char.MaxValue];
+            for (var i = 0; i < convertedString.Length; ++i)
+            {
+                ++count[convertedString[i]];
+            }
+            var sum = 0;
+            for (var i = 0; i < (int)char.MaxValue; ++i) 
+            { 
+                sum += count[i];
+                count[i] = sum - count[i];
+            }
+            var t = new int[convertedString.Length];
+            for (var i = 0; i < convertedString.Length; ++i)
+            {
+                t[count[convertedString[i]]] = i;
+                count[convertedString[i]]++;
+            }
+            var j = t[tableIndex];
+            var answer = new char[convertedString.Length];
+            for (int i = 0; i < convertedString.Length; ++i)
+            {
+                answer[i] = convertedString[j];
+                j = t[j];
+            }
+
+            return string.Join("", answer);
         }
         static void Main(string[] args)
         {
-            var str = "123456";
-            int resultIndex = 0;
-            Console.WriteLine(DirectConversion(str, ref resultIndex));
-            Console.WriteLine(resultIndex);
+            var str = "ABACABA";
+            var resultIndex = 0;
+            var newStr = DirectConversion(str, ref resultIndex);
+            //Console.WriteLine(resultIndex);
+            Console.WriteLine(ReverseConversion(newStr, resultIndex));
         }
+
     }
 }
