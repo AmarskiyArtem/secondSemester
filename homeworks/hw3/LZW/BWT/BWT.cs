a09b0d387e363632d9f407ca3ccc4f547ed78e32
AmarskiyArtem <amarskiywork@gmail.com>
2023-03-15 16:58:08 +0300

    add project files, add trie and bwt libraries

---
+++ homeworks/hw3/LZW/BWT/BWT.cs
@@ -0,0 +1,65 @@
+﻿namespace BWTransform;
+
+// A static class that implements direct and reverse Burrows–Wheeler transform
+public static class BWT
+{
+    // Direct BWT, returns string after conversion and index of original string 
+    // in shift table
+    static (string, int) SlowDirectConversion(string originalString)
+    {
+        var shifts = originalString;
+        var shiftTable = new string[originalString.Length];
+        shiftTable[0] = originalString;
+        for (var i = 1; i < shiftTable.Length; i++)
+        {
+            char firstChar = shifts[0];
+            shifts = shifts.Substring(1) + firstChar;
+            shiftTable[i] = shifts;
+        }
+        Array.Sort(shiftTable, StringComparer.Ordinal);
+        string result = "";
+        for (var i = 0; i < shiftTable.Length; i++)
+        {
+            result += shiftTable[i][shiftTable.Length - 1];
+        }
+        return (result, Array.IndexOf(shiftTable, originalString));
+    }
+
+    private const int alphabetSize = 65536;
+
+    // Reverse BWT, takes string after BWT and index of original string in sorted shifts table,
+    // returns original string
+    static string ReverseConversion(string convertedString, int tableIndex)
+    {
+        var countSortArray = new int[alphabetSize];
+        for (var i = 0; i < convertedString.Length; ++i)
+        {
+            ++countSortArray[convertedString[i]];
+        }
+
+        // Sort symbols to get first column of original table
+        //countSortArray[i] to first position i-symbol in 1st column
+        var sum = 0;
+        for (var i = 0; i < alphabetSize; ++i)
+        {
+            sum += countSortArray[i];
+            countSortArray[i] = sum - countSortArray[i];
+        }
+
+        var nextSymbolIndex = new int[convertedString.Length];
+        for (var i = 0; i < nextSymbolIndex.Length; ++i)
+        {
+            nextSymbolIndex[countSortArray[convertedString[i]]] = i;
+            countSortArray[convertedString[i]]++;
+        }
+
+        var nextSymbol = nextSymbolIndex[tableIndex];
+        var answer = new char[convertedString.Length];
+        for (int i = 0; i < convertedString.Length; ++i)
+        {
+            answer[i] = convertedString[nextSymbol];
+            nextSymbol = nextSymbolIndex[nextSymbol];
+        }
+        return new string(answer);
+    }
+}
---
+++ homeworks/hw3/LZW/Trie/Trie.cs
@@ -0,0 +1,127 @@
+﻿namespace TrieLibrary;
+
+using System.Collections.Generic;
+
+// Trie data structure
+public class Trie
+{
+    private class TrieNode
+    {
+        public Dictionary<char, TrieNode> Children { get; set; } = new();
+
+        public bool IsTerminate { get; set; }
+
+        public int WordsWithSamePrefix { get; set; }
+    }
+
+    private readonly TrieNode root = new();
+
+    private bool isEmptyStringAdded;
+
+    public int Size { get; private set; }
+
+    // Add word to trie. Returns false if the word already been added otherwise true
+    public bool Add(string word)
+    {
+        if (word == "")
+        {
+            if (isEmptyStringAdded)
+            {
+                return false;
+            }
+            isEmptyStringAdded = true;
+            this.Size++;
+            return true;
+        }
+
+        if (Contains(word))
+        {
+            return false;
+        }
+
+        TrieNode node = this.root;
+        foreach (var ch in word)
+        {
+            if (!node.Children.ContainsKey(ch))
+            {
+                node.Children[ch] = new TrieNode();
+            }
+            node.WordsWithSamePrefix++;
+            node = node.Children[ch];
+        }
+        node.WordsWithSamePrefix++;
+        node.IsTerminate = true;
+        this.Size++;
+        return true;
+    }
+
+    // Check if word contatins in the trie
+    public bool Contains(string word)
+    {
+        if (word == "")
+        {
+            return isEmptyStringAdded;
+        }
+
+        TrieNode node = this.root;
+        foreach (var ch in word)
+        {
+            if (!node.Children.ContainsKey(ch))
+            {
+                return false;
+            }
+            node = node.Children[ch];
+        }
+        return node.IsTerminate;
+    }
+
+    // Remove element from the trie. Returns true if successful, false if element 
+    // is not contained in the trie
+    public bool Remove(string word)
+    {
+        if (word == "")
+        {
+            if (isEmptyStringAdded)
+            {
+                isEmptyStringAdded = false;
+                return true;
+            }
+            return false;
+        }
+
+        if (!Contains(word))
+        {
+            return false;
+        }
+
+        TrieNode node = this.root;
+        foreach (var ch in word)
+        {
+            node.WordsWithSamePrefix--;
+            node = node.Children[ch];
+        }
+        node.IsTerminate = false;
+        this.Size--;
+        return true;
+
+    }
+
+    // Return how many word added to the trie starts with the passed prefix
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        if (prefix == "")
+        {
+            return Convert.ToInt32(isEmptyStringAdded);
+        }
+        TrieNode node = this.root;
+        foreach (var ch in prefix)
+        {
+            if (!node.Children.ContainsKey(ch))
+            {
+                return 0;
+            }
+            node = node.Children[ch];
+        }
+        return node.WordsWithSamePrefix;
+    }
+}
