fe3d011aaf7afaff34717ade9b52486900b15100
AmarskiyArtem <amarskiywork@gmail.com>
2023-03-15 22:24:20 +0300

    add and test push, insert, Get List methods

---
+++ homeworks/hw4/UniqueList/UniqueList/List.cs
@@ -0,0 +1,67 @@
+﻿namespace UniqueListLibrary;
+
+public class List
+{
+    private class ListElement
+    {
+
+        public ListElement(int value, ListElement? next)
+        {
+            this.Value = value;
+            this.Next = next;
+        }
+        public int Value { get; set; }
+        public ListElement? Next { get; set; }
+    }
+
+    public int Size { get; private set; }
+
+    private ListElement? Head;
+
+    public void Push(int value)
+    {
+        var newHead = new ListElement(value, this.Head);
+        this.Head = newHead;
+        Size++;
+    }
+
+    public int Get(int index)
+    {
+        if (index >= Size)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        var currentListElement = Head;
+        for (var i = 0; i < index; i++)
+        {
+            currentListElement = currentListElement!.Next;
+        }
+
+        return currentListElement!.Value;
+    }
+    
+    public void Insert(int value, int index)
+    {
+        if (index > Size)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        if (index == 0)
+        {
+            Push(value);
+            return;
+        }
+
+        var currentListElement = Head;
+        for (var i = 0; i < index - 1; i++)
+        {
+            currentListElement = currentListElement!.Next;
+        }
+
+        var newElement = new ListElement(value, currentListElement!.Next);
+        currentListElement.Next = newElement;
+        Size++;
+    }
+}
