703a24515ccd5d239a5afb340bc1f30be376e0bd
AmarskiyArtem <amarskiywork@gmail.com>
2023-03-20 13:44:35 +0300

    add operand implementation

--- homeworks/hw4/ParsingTree/ParsingTree/Operand.cs
+++ homeworks/hw4/ParsingTree/ParsingTree/Operand.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ParsingTreeLibrary;
 
-namespace ParsingTree
+internal class Operand : INode
 {
-    internal class Operand
+    public Operand(int value)
     {
+        this.Value = value;
+    }
+
+    public int Value { get; }
+
+    public void Print()
+    {
+        Console.WriteLine(Value);
+    }
+
+    public double Calculate()
+    {
+        return Value;
     }
 }
--- homeworks/hw4/ParsingTree/ParsingTree/Operation.cs
+++ homeworks/hw4/ParsingTree/ParsingTree/Operation.cs
@@ -2,5 +2,11 @@
 
 internal abstract class Operation : INode
 {
+    public INode leftChild { get; set; }
 
+    public INode rightChild { get; set; }
+
+    public abstract void Print();
+
+    public abstract double Calculate();
 }
