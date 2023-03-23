6eb7812a218c2b25085fde8f42c4127cddba8879
AmarskiyArtem <amarskiywork@gmail.com>
2023-03-23 12:21:15 +0300

    add CompressByteBuffer.cs

---
+++ homeworks/hw3/LZW/LZW/Buffers/CompressByteBuffer.cs
@@ -0,0 +1,12 @@
+﻿namespace LZW.Buffers;
+
+internal class CompressByteBuffer
+{
+    public List<byte> Data { get; private set; } = new();
+
+    public int CurrentNumberOfBitsPerChar { get; set; } = 9;
+
+    public byte CurrentByte { get; private set; }
+
+    
+}
--- homeworks/hw3/LZW/LZW/LZWEncoder.cs
+++ homeworks/hw3/LZW/LZW/LZWEncoder.cs
@@ -4,30 +4,18 @@ using TrieLibrary;
 
 internal static class LZWEncoder
 {
-    public static byte[] Encode(byte[] data)
+    private static Trie InitTrie()
     {
-        var trie = new Trie();
-        for (var i = 0; i < 256; i++) 
+        Trie trie = new Trie();
+        for (var i = 0; i < 256; i++)
         {
             trie.Add(new List<byte> { (byte)i }, i);
         }
-        var output = new List<byte>();
-        var currentSequence = new List<byte>();
-        for (var i = 0; i < data.Length; i++)
-        {
-            var newSymbols = new List<byte>();
-            newSymbols.AddRange(currentSequence);
-            newSymbols.Add(data[i]);
-            if (trie.Contains(newSymbols))
-            {
-                currentSequence = newSymbols;
-            }
-            else
-            {
-                var value = trie.Get(currentSequence);
-
-            }
-        }
-        return new byte[5];
+        return trie;
+    }
+    public static byte[] Encode(byte[] data)
+    {
+        var trie = InitTrie();
+        
     }
 }
