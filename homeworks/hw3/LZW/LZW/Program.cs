1283bbc06c9057cf89814cbbfa307d9cd1e70257
AmarskiyArtem <amarskiywork@gmail.com>
2023-03-25 01:33:35 +0300

    delete excess code

--- homeworks/hw3/LZW/LZW/Program.cs
+++ homeworks/hw3/LZW/LZW/Program.cs
@@ -1,85 +1,39 @@
 ﻿using LZW;
-using System.Runtime.Serialization.Formatters;
 
-//if (args.Length < 2)
-//{
-//    Console.WriteLine("Need more arguments");
-//}
-//else
-//{
-//    if (args[1] == "--c")
-//    {
-//        try
-//        {
-//            Console.WriteLine("Zipping...");
-//            LZWTransform.Compress(args[0]);
-//            Console.WriteLine("Successfully");
-//        }
-//        catch (FileNotFoundException)
-//        {
-//            Console.WriteLine("File not found");
-//        }
-//    }
-//    else if (args[1] == "--u")
-//    {
-//        try
-//        {
-//            Console.WriteLine("Unzipping...");
-//            LZWTransform.Decompress(args[0]);
-//            Console.WriteLine("Successfully");
-//        }
-//        catch (FileNotFoundException)
-//        {
-//            Console.WriteLine("File not found");
-//        }
-//    }
-//    else
-//    {
-//        Console.WriteLine("Incorrect argument");
-//    }
-//}
-
-
-//LZWTransform.Compress(@"D:\games\Vampire.Survivors.Build.10331827\VampireSurvivors.exe");
-//LZWTransform.Decompress(@"D:\games\Vampire.Survivors.Build.10331827\VampireSurvivors.exe.zipped");
-//LZWTransform.Compress(@"D:\tests\TextDocument.txt");
-//LZWTransform.Decompress(@"D:\tests\TextDocument.txt.zipped");
-//LZWTransform.Decompress(@"D:\tests\TextDocument.txt.zipped");
-
-
-
-LZWTransform.Compress(@"D:\tests\video_2023-03-25_01-24-08.mp4");
-LZWTransform.Decompress(@"D:\tests\video_2023-03-25_01-24-08.mp4.zipped");
-
-//var data1 = File.ReadAllBytes(@"D:\tests\Screenshot 2023-03-15 204216.png");
-//var data2 = File.ReadAllBytes(@"C:\Users\AmarskiyArtem\Pictures\Screenshots\Screenshot 2023-03-15 204216.png");
-
-//for (var i = 0; i < data1.Length; i++)
-//{
-//    if (data1[i] != data2[i])
-//    {
-//        Console.WriteLine("Bad");
-//        Console.ReadKey();
-//    }
-//}
-
-//for (var i = 0; i < data2.Length; i++)
-//{
-//    if (data1[i] != data2[i])
-//    {
-//        Console.WriteLine("Bad");
-//        Console.ReadKey();
-//    }
-//}
-//Console.WriteLine("Nice");
-//for (int i = 535510;  i < data1.Length; i++)
-//{
-//    Console.Write(data1[i] + " ");
-//}
-
-//Console.WriteLine();
-
-//for (int i = 535510; i < data2.Length; i++)
-//{
-//    Console.Write(data2[i] + " ");
-//}
+if (args.Length < 2)
+{
+    Console.WriteLine("Need more arguments");
+}
+else
+{
+    if (args[1] == "--c")
+    {
+        try
+        {
+            Console.WriteLine("Zipping...");
+            LZWTransform.Compress(args[0]);
+            Console.WriteLine("Successfully");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found");
+        }
+    }
+    else if (args[1] == "--u")
+    {
+        try
+        {
+            Console.WriteLine("Unzipping...");
+            LZWTransform.Decompress(args[0]);
+            Console.WriteLine("Successfully");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Incorrect argument");
+    }
+}
