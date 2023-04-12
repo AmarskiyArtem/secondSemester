3dfb4a13862d0ab01792a3a8e76e329ad45448f3
AmarskiyArtem <amarskiywork@gmail.com>
2023-04-12 16:07:07 +0300

    Complete Map with generic

---
+++ homeworks/hw6/LambdaPractice/LambdaPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using LambdaPractice;
+
+var lst  = new List<int>() 
+{
+    0, 1, 2, 3, 4, 5,
+};
+
+lst = Funcs<int>.Map(lst, x => x * 2);
+lst.Clear();
---
+++ homeworks/hw6/LambdaPractice/LambdaPractice/funcs.cs
@@ -0,0 +1,14 @@
+﻿namespace LambdaPractice;
+
+internal class Funcs <T>
+{
+    public static List<T> Map(List<T> list, Func<T, T> func)
+    {
+        var result = new List<T>();
+        foreach (var x in list)
+        {
+            result.Add(func(x));
+        }
+        return result;
+    }
+}
