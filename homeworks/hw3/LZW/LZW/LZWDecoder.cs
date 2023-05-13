2c15fbc7ca4526c2061d1c9e931b104fe44f499d
Artem <amarskiywork@gmail.com>
2023-05-13 12:38:37 +0300

    delete partial classes

--- homeworks/hw3/LZW/LZW/LZWDecoder.cs
+++ homeworks/hw3/LZW/LZW/LZWDecoder.cs
@@ -19,8 +19,97 @@ namespace LZW;
 /// <summary>
 /// Class for LZW (with BWT) decode, works with byte[]
 /// </summary>
-public partial class LZWDecoder
+public static class LZWDecoder
 {
+    private class DecompressByteBuffer
+    {
+        /// <summary>
+        /// output
+        /// </summary>
+        public List<int> Data { get; private set; } = new();
+
+        public int CurrentBitsPerNumber { get; set; } = 9;
+
+        public int CurrentNumber { get; private set; }
+        public int BitsInCurrentNumber { get; private set; }
+
+        private const int BitsInByte = 8;
+
+        /// <summary>
+        /// Takes byte, adds to current buffer
+        /// </summary>
+        /// <param name="bt">byte</param>
+        /// <returns>return if new number was added to output</returns>
+        public bool AddByteToData(byte bt)
+        {
+            bool wasAdded = false;
+            var bits = GetBitsFromByte(bt);
+            foreach (var bit in bits)
+            {
+                CurrentNumber = (CurrentNumber << 1) + bit;
+                ++BitsInCurrentNumber;
+                if (BitsInCurrentNumber == CurrentBitsPerNumber)
+                {
+                    AddNumberToData();
+                    wasAdded = true;
+                }
+            }
+            return wasAdded;
+        }
+
+        private void AddNumberToData()
+        {
+            Data.Add(CurrentNumber);
+            CurrentNumber = 0;
+            BitsInCurrentNumber = 0;
+        }
+
+        private static byte[] GetBitsFromByte(byte bt)
+        {
+            var bits = new byte[BitsInByte];
+            for (var i = BitsInByte - 1; i >= 0; i--)
+            {
+                bits[i] = (byte)(bt % 2);
+                bt >>= 1;
+            }
+            return bits;
+        }
+
+        /// <summary>
+        /// Using for cases if in LZWEncode wasnt enough bits for added (added extra zeros)
+        /// </summary>
+        /// <param name="bt">last byte</param>
+        public void AddLastByteToData(byte bt)
+        {
+            var bits = BitsOfLastByte(bt);
+            foreach (var bit in bits)
+            {
+                CurrentNumber = (CurrentNumber << 1) + bit;
+                ++BitsInCurrentNumber;
+                if (BitsInCurrentNumber == CurrentBitsPerNumber)
+                {
+                    AddNumberToData();
+                }
+            }
+        }
+
+        private byte[] BitsOfLastByte(byte bt)
+        {
+            var bits = new List<byte>();
+            while (bt > 0)
+            {
+                bits.Add((byte)(bt % 2));
+                bt >>= 1;
+            }
+            while (bits.Count + BitsInCurrentNumber < BitsInByte)
+            {
+                bits.Add(0);
+            }
+            bits.Reverse();
+            return bits.ToArray();
+        }
+
+    }
     const int maxAmountOfRecords = 65536;
 
     /// <summary>
--- homeworks/hw3/LZW/LZW/LZWEncoder.cs
+++ homeworks/hw3/LZW/LZW/LZWEncoder.cs
@@ -21,8 +21,65 @@ using TrieLibrary;
 /// <summary>
 /// Class for LZW (optional with BWT) encode, works with byte[]
 /// </summary>
-public static partial class LZWEncoder
+public static class LZWEncoder
 {
+    /// <summary>
+    /// Byte buffer for LZWEncoder
+    /// </summary>
+    private class CompressByteBuffer
+    {
+        /// <summary>
+        /// output
+        /// </summary>
+        public List<byte> Data { get; private set; } = new();
+
+        public int CurrentNumberOfBitsPerNumber { get; set; } = 9;
+
+        public byte CurrentByte { get; private set; } = 0;
+
+        private int bitsInCurrentByte = 0;
+
+        private const int BitsInByte = 8;
+
+        /// <summary>
+        /// Adds number to output
+        /// </summary>
+        /// <param name="number"></param>
+        public void AddNumberToData(int number)
+        {
+            var bits = GetBitsFromNumber(number);
+            foreach (var bit in bits)
+            {
+                CurrentByte = (byte)((CurrentByte << 1) + bit);
+                bitsInCurrentByte++;
+                if (bitsInCurrentByte == BitsInByte)
+                {
+                    AddByteToData();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add current byte to output
+        /// </summary>
+        public void AddByteToData()
+        {
+            Data.Add(CurrentByte);
+            CurrentByte = 0;
+            bitsInCurrentByte = 0;
+        }
+
+        private byte[] GetBitsFromNumber(int number)
+        {
+            var bits = new byte[CurrentNumberOfBitsPerNumber];
+            for (var i = CurrentNumberOfBitsPerNumber - 1; i >= 0; i--)
+            {
+                bits[i] = (byte)(number % 2);
+                number >>= 1;
+            }
+            return bits;
+        }
+    }
     private static Trie FillTrieBySingleByteSequences()
     {
         var trie = new Trie();
