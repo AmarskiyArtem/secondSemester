f4e5c87911df577482827e61c2d1a7c6f926b8f4
Artem <amarskiywork@gmail.com>
2023-05-16 15:31:30 +0300

    fix ci/cd

---
+++ homeworks/hw7/WinFormsCalculator/Calculator/Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace CalculatorLibrary;
+
+using System.ComponentModel;
+
+public class Calculator : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
