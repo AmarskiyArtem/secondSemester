253bc4839f6d6ec9244581d11b43292763a0c145
Artem <amarskiywork@gmail.com>
2023-05-16 23:13:58 +0300

    add func in calc class

--- homeworks/hw7/WinFormsCalculator/Calculator/Calculator.cs
+++ homeworks/hw7/WinFormsCalculator/Calculator/Calculator.cs
@@ -4,5 +4,67 @@ using System.ComponentModel;
 
 public class Calculator : INotifyPropertyChanged
 {
+    private enum states
+    {
+        NumberIsTyping,
+        OperationArePressed,
+        SomethingWentWrong,
+    }
+
+    private states state = states.NumberIsTyping;
+
+    private string currentResult = "0";
+
+    public string CurrentResult
+    {
+        get
+        { 
+            return currentResult; 
+        }
+        set
+        {
+            currentResult = value;
+            notifyProperty();
+        }
+    }
+
     public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void notifyProperty(string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private string intermediateResult = "0";
+
+    private char operation = ' ';
+
+    public void AddNumber(char digit)
+    {
+        if (!char.IsDigit(digit))
+        {
+            throw new ArgumentException();
+        }
+
+        switch (state)
+        {
+            case states.SomethingWentWrong:
+            case states.NumberIsTyping:
+                if (currentResult == "0" || currentResult == "Error") 
+                {
+                    currentResult = digit.ToString();
+                }
+                else
+                {
+                    currentResult += digit;
+                }
+                state = states.NumberIsTyping;
+                break;
+            case states.OperationArePressed:
+                intermediateResult = currentResult;
+                currentResult = digit.ToString();
+                state = states.NumberIsTyping;
+                break;
+        }
+    }
 }
