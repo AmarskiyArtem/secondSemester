7b3e055607bf83af1f436ac72997a8b19c4a50a4
Artem <amarskiywork@gmail.com>
2023-05-17 12:56:18 +0300

    add compute and add num/operation funcs

--- homeworks/hw7/WinFormsCalculator/Calculator/Calculator.cs
+++ homeworks/hw7/WinFormsCalculator/Calculator/Calculator.cs
@@ -37,7 +37,7 @@ public class Calculator : INotifyPropertyChanged
 
     private string intermediateResult = "0";
 
-    private char operation = ' ';
+    private char currentOperation = ' ';
 
     public void AddNumber(char digit)
     {
@@ -64,6 +64,100 @@ public class Calculator : INotifyPropertyChanged
                 intermediateResult = currentResult;
                 currentResult = digit.ToString();
                 state = states.NumberIsTyping;
+                break;
+        }
+    }
+
+    public void AddOperation(char operation)
+    {
+        if (!"+-*/".Contains(operation))
+        {
+            throw new ArgumentException();
+        }
+
+        switch (state)
+        {
+            case states.NumberIsTyping:
+                if (operation == ' ')
+                {
+                    intermediateResult = currentResult;
+                    currentOperation = operation;
+                    state = states.OperationArePressed;
+                }
+                else
+                {
+                    try
+                    {
+                        currentResult = compute().ToString();
+                    }
+                    catch (Exception e) when (e is ArgumentException || e is DivideByZeroException)
+                    {
+                        Clear();
+                        currentResult = "Error";
+                        state = states.SomethingWentWrong;
+                        return;
+                    }
+                    intermediateResult = currentResult;
+                    currentOperation = operation;
+                    state = states.OperationArePressed;
+                }
+                break;
+            case states.OperationArePressed:
+                currentOperation = operation;
+                break;
+            case states.SomethingWentWrong:
+                break;
+
+        }
+    }
+
+    private double compute()
+    {
+        switch (currentOperation)
+        {
+            case '+':
+                return double.Parse(intermediateResult) + double.Parse(currentResult);
+            case '-':
+                return double.Parse(intermediateResult) - double.Parse(currentResult);
+            case '*':
+                return double.Parse(intermediateResult) - double.Parse(currentResult);
+            case '/':
+                if (Math.Abs(double.Parse(currentResult)) < 0.001)
+                {
+                    throw new DivideByZeroException();
+                }
+                return double.Parse(intermediateResult) / double.Parse(currentResult);
+            default: throw new ArgumentException();
+        }
+    }
+
+    public void Clear()
+    {
+        currentResult = "0";
+        intermediateResult = "0";
+        currentOperation = ' ';
+        state = states.NumberIsTyping;
+    }
+
+    public void Calculate()
+    {
+        double result = 0;
+
+        switch (state)
+        {
+            case states.OperationArePressed:
+                try
+                {
+                    result = compute();
+                }
+                catch (Exception e) when (e is ArgumentException || e is DivideByZeroException)
+                {
+                    Clear();
+                    currentResult = "Error";
+                    state = states.SomethingWentWrong;
+                    return;
+                }
+                currentResult = result.ToString();
                 break;
         }
     }
