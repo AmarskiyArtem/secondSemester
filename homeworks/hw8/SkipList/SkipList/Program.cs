187647bcc9d5a2fa4c1d749da4f67611974f69e8
Artem <amarskiywork@gmail.com>
2023-05-26 00:09:32 +0300

    add SkipList implementation

--- homeworks/hw8/SkipList/SkipList/Program.cs
+++ homeworks/hw8/SkipList/SkipList/Program.cs
@@ -1,3 +1,9 @@
 ﻿using SkipListLibrary;
 
-Console.WriteLine(default(int));
+var skipList  = new SkipList<int>();
+skipList.Add(0);
+skipList.Add(1);
+skipList.Add(2);
+skipList.Add(3);
+skipList.Add(4);
+skipList.Add(5);
--- homeworks/hw8/SkipList/SkipList/SkipList.cs
+++ homeworks/hw8/SkipList/SkipList/SkipList.cs
@@ -1,71 +1,267 @@
-﻿using System.Collections;
+﻿namespace SkipListLibrary;
 
-namespace SkipListLibrary;
+using System.Collections;
 
 public class SkipList<T> : IList<T>
     where T : IComparable
 {
-    private record Node(T? Value, Node[] Next);
+    static readonly double newLevelChance = 0.5;
+    static readonly int maxLevel = 24;
+    static readonly Random random = new();
 
-    private readonly Node tail = new(default, Array.Empty<Node>());
+    internal int version = 0;
+    internal Node head = new(default, maxLevel);
+    private int currentMaxLevel = 0;
 
-    private Node head = new(default, new Node[1]);
+    internal class Node
+    {
+        public Node(T? value, int level)
+        {
+            Value = value;
+            Next = new Node[level];
+        }
+        public T? Value { get; } 
+        public Node[] Next { get; set; }
+    }
     
 
+    public T this[int index] 
+    {
+        get
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            var currentNode = head;
+            for (var i = 0; i <= index; ++i)
+            {
+                currentNode = currentNode.Next[0];
+            }
+            if (currentNode.Value is null)
+            {
+                throw new InvalidOperationException();
+            }
+            return currentNode.Value;
+        }
+        set => throw new NotSupportedException("//TODO");
+    }
 
-    public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int Count { get; private set; }
 
-    public int Count => throw new NotImplementedException();
 
     bool ICollection<T>.IsReadOnly => false;
 
-    public void Add(T item)
+    public void Add(T value)
     {
-        throw new NotImplementedException();
+        var newNodeLevel = RandomLevel();
+        var newNode = new Node(value, newNodeLevel);
+        var currentNode = head;
+        for (var level = currentMaxLevel - 1; level >= 0; level--)
+        {
+            while (currentNode.Next[level] != null)
+            {
+                if (value.CompareTo(currentNode.Next[level].Value) < 0)
+                {
+                    break;
+                }
+                currentNode = currentNode.Next[level];
+            }
+            if (level < newNodeLevel)
+            {
+                newNode.Next[level] = currentNode.Next[level];
+                currentNode.Next[level] = newNode;
+            }
+        }
+        Count++;
+        version++;
     }
 
     public void Clear()
     {
-        throw new NotImplementedException();
+        currentMaxLevel = 1;
+
+        head = new(default, maxLevel);
+        version++;
+        Count = 0;
     }
 
     public bool Contains(T item)
     {
-        throw new NotImplementedException();
+        var currentNode = head;
+        for (var level = currentMaxLevel - 1; level >= 0; level--)
+        {
+            while (currentNode.Next[level] != null)
+            {
+                if (item.CompareTo(currentNode.Next[level].Value) < 0)
+                {
+                    break;
+                }
+                if (item.CompareTo(currentNode.Next[level].Value) == 0)
+                {
+                    return true;
+                }
+
+                currentNode = currentNode.Next[level];
+            }
+        }
+        return false;
     }
 
     public void CopyTo(T[] array, int arrayIndex)
     {
-        throw new NotImplementedException();
+        if (arrayIndex >= Count || array.Length < Count - arrayIndex)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var currentNode = head;
+        for (var i = 0; i < arrayIndex; ++i)
+        {
+            currentNode = currentNode.Next[0];
+        }
+
+        for (var i = 0; i < array.Length; ++i)
+        {
+            array[i] = currentNode.Next[0].Value!;
+            currentNode = currentNode.Next[0];
+        }
     }
 
-    public IEnumerator<T> GetEnumerator()
+    public int IndexOf(T value)
     {
-        throw new NotImplementedException();
-    }
-
-    public int IndexOf(T item)
-    {
-        throw new NotImplementedException();
+        var counter = 0;
+        var currentNode = head;
+        while (currentNode.Next[0] != null)
+        {
+            if (value.CompareTo(currentNode.Next[0].Value) == 0)
+            {
+                return counter;
+            }
+            counter++;
+            currentNode = currentNode.Next[0];
+        }
+        return -1;
     }
 
     public void Insert(int index, T item)
     {
-        throw new NotSupportedException();
+        throw new NotSupportedException("//TODO");
     }
 
-    public bool Remove(T item)
+    public bool Remove(T value)
     {
-        throw new NotImplementedException();
+        var success = false;
+        var currentNode = head;
+        for (var level = currentMaxLevel - 1; level >= 0; level--)
+        {
+            while (currentNode.Next[level] != null)
+            {
+                if (value.CompareTo(currentNode.Next[level].Value) < 0)
+                {
+                    break;
+                }
+                if (value.CompareTo(currentNode.Next[level].Value) == 0)
+                {
+                    currentNode.Next[level] = currentNode.Next[level].Next[level];
+                    success = true;
+                    break;
+                }
+                currentNode = currentNode.Next[level];
+            }
+        }
+        if (success)
+        {
+            Count--;
+        }
+        version++;
+        return success;
     }
 
     public void RemoveAt(int index)
+        => Remove(this[index]);
+
+    public struct Enumerator : IEnumerator<T>, IEnumerator
     {
-        throw new NotImplementedException();
+        private readonly int version;
+        SkipList<T> skipList;
+        Node currentNode;
+        T? currentValue;
+
+        internal Enumerator(SkipList<T> skipList)
+        {
+            this.skipList = skipList;
+            currentNode = skipList.head;
+            version = skipList.version;
+            currentValue = default;
+        }
+
+        public T Current => currentValue!;
+
+        object? IEnumerator.Current
+        {
+            get
+            {
+                if (currentNode.Next == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                return Current;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (version != skipList.version)
+            {
+                throw new InvalidOperationException();
+            }
+            if (currentNode.Next[0] == null)
+            {
+                currentNode = skipList.head;
+                currentValue = default;
+                return false;
+            }
+
+            currentNode = currentNode.Next[0];
+            currentValue = currentNode.Value;
+            return true;
+        }
+
+        public void Reset()
+        {
+            if (version != skipList.version)
+            {
+                throw new InvalidOperationException();
+            }
+
+            currentNode = skipList.head;
+            currentValue = default;
+        }
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
+    private int RandomLevel()
     {
-        throw new NotImplementedException();
+        var resultLevel = 1;
+        while (random.NextDouble() < newLevelChance && resultLevel < maxLevel)
+        {
+            resultLevel++;
+        }
+        currentMaxLevel = Math.Max(currentMaxLevel, resultLevel);
+        return resultLevel;
     }
+
+    public Enumerator GetEnumerator()
+        => new(this);
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        => GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => ((IEnumerable<T>)this).GetEnumerator();
+
 }
