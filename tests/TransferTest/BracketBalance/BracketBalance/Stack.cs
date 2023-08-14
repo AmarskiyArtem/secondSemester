0397a7538d3f9237f627e4fee4b94b733b772916
Artem <amarskiywork@gmail.com>
2023-08-14 13:59:25 +0300

    task2 add Stack.cs

---
+++ tests/TransferTest/BracketBalance/BracketBalance/Stack.cs
@@ -0,0 +1,34 @@
+﻿namespace BracketBalance;
+
+using System.Collections.Generic;
+
+public class Stack<T>
+{
+    public Stack()
+        => this.data = new();
+
+    private readonly List<T> data;
+    public void Push(T value)
+        => this.data.Insert(0, value);
+
+    public void Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        this.data.RemoveAt(0);
+    }
+
+    public T Top()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        return this.data[0];
+    }
+
+    public bool IsEmpty()
+        => this.data.Count == 0;
+}
