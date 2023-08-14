fba30dd78a72a27e41505b1e894d000b7b32879f
Artem <amarskiywork@gmail.com>
2023-08-14 14:10:39 +0300

    add task2 solution

---
+++ tests/TransferTest/BracketBalance/BracketBalance/ValidBrackerSequence.cs
@@ -0,0 +1,44 @@
+﻿namespace BracketBalance;
+
+public static class ValidBrackerSequence
+{
+    public static bool IsValidBracketSequence(string sequence)
+    {
+        var stack = new Stack<char>();
+
+        foreach (var bracket in sequence)
+        {
+            if (bracket == '(' || bracket == '[' || bracket == '<')
+            {
+                stack.Push(bracket);
+            }
+            else if (bracket == ')' || bracket == ']' || bracket == '>')
+            {
+                if (stack.IsEmpty())
+                {
+                    return false;
+                }
+
+                var openBracket = stack.Top();
+                stack.Pop();
+
+                if ((bracket == ')' && openBracket != '(') ||
+                    (bracket == ']' && openBracket != '[') ||
+                    (bracket == '>' && openBracket != '<'))
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return stack.IsEmpty();
+    }
+}
+
