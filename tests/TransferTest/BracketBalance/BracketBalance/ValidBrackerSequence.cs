0955e88e95b640073ceb5dcd364e976c909ae32e
Artem <amarskiywork@gmail.com>
2023-08-14 14:39:11 +0300

    add xml documetation

--- tests/TransferTest/BracketBalance/BracketBalance/ValidBrackerSequence.cs
+++ tests/TransferTest/BracketBalance/BracketBalance/ValidBrackerSequence.cs
@@ -1,7 +1,15 @@
 ﻿namespace BracketBalance;
 
+/// <summary>
+/// Provides methods for validating a bracket sequence.
+/// </summary>
 public static class ValidBrackerSequence
 {
+    /// <summary>
+    /// Validates whether the given string contains a valid bracket sequence.
+    /// Empty string it is considered correct bracket sequence.
+    /// </summary>
+    /// <param name="sequence">The string containing the bracket sequence to be validated.</param>
     public static bool IsValidBracketSequence(string sequence)
     {
         var stack = new Stack<char>();
---
+++ tests/TransferTest/BracketBalance/BracketBalanceTests/StackTests.cs
@@ -0,0 +1,42 @@
+﻿namespace BracketBalance.Tests;
+public class StackTests
+{
+
+    Stack<int> stack = new();
+
+    [SetUp]
+    public void Setup()
+    {
+        stack = new();
+    }
+
+    [Test]
+    public void PushAndTopShouldSameValue()
+    {
+        stack.Push(1);
+        Assert.That(stack.Top, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TwoPushAndTwoTopShouldSameValue()
+    {
+        stack.Push(2);
+        stack.Push(3);
+        Assert.That(stack.Top, Is.EqualTo(3));
+        stack.Pop();
+        Assert.That(stack.Top, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void TopFromEmptyStackShouldException()
+    {
+        Assert.Throws<InvalidOperationException>(() => stack.Top());
+    }
+
+    [Test]
+    public void PopFromEmptyStackShouldException()
+    {
+        Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+
+}
